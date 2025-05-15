de765e9c415b8ac6316361f976da872e13b36e41
marcelo_cardoso <marcelosouza7r@gmail.com>
2025-05-14 21:48:47 -0300

    Add method that implement bubble sort algorithm

--- main.py
+++ main.py
@@ -6,4 +6,22 @@ def is_sorted(input_list: list[int]) -> bool:
             return False
     return True
 
-print(is_sorted(input_list=[1, 2, 3, 4, 5]))
+def bubble_sort(input_list: list[int]) -> list[int]:
+    iteration = 0
+    while not is_sorted(input_list):
+        iteration += 1
+        for index in range(1, len(input_list)):
+            max_value = input_list[index]
+            if input_list[index] < input_list[index-1]:
+                input_list[index] = input_list[index - 1]
+                input_list[index - 1] = max_value
+
+        print(f"List after iteration {iteration}: {input_list}")
+
+    return input_list
+
+
+if __name__ == '__main__':
+    input_list = [5, 4, 3, 2, 1]
+    input_list_sorted = bubble_sort(input_list.copy())
+    print(f"The list {input_list} sorted: {input_list_sorted}")
